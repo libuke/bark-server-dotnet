184732d1ff7e993fe75f651ea23ef5faea67f3aa
libuke025 <libuke025@gmail.com>
2021-07-20 23:00:28 +0800

    update message model

--- BarkServerNet/Models/Message.cs
+++ BarkServerNet/Models/Message.cs
@@ -7,10 +7,10 @@ namespace BarkServerNet
         [Required]
         public string DeviceKey { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
-        [Required]
-        public string Body { get; set; }
+        public string Body { get; set; } = "";
 
         public string Group { get; set; }
     }
